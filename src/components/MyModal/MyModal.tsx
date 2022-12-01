a3cfe537406f4b5db977ac31a580678c775b9435
chemiic <nosach.pavel@yandex.ru>
2022-12-01 19:51:38 +0300

    sss

---
+++ src/components/MyModal/MyModal.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+import styles from './MyModal.module.scss'
+const MyModal: FC = () => {
+
+    return(
+        <div className={styles.MyModal}>
+
+        </div>
+    )
+}
+export default MyModal

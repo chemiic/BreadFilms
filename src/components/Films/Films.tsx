4b8eed41cd1a0199a39700d00885ff1ed756e547
chemiic <nosach.pavel@yandex.ru>
2023-01-10 16:48:47 +0300

    1we

--- src/components/Films/Films.tsx
+++
@@ -1,9 +0,0 @@
-// import {FilmData} from "../../assets/store/storeFilm";
-// import FilmCard from "../FIlmCard/FilmCard";
-//
-// function Films(){
-//     return(
-//
-//     )
-// }
-// export default Films

b02d9d6f0cc5310dbbce5e7923660ef47110c609
chemiic <nosach.pavel@yandex.ru>
2023-01-10 16:33:04 +0300

    fix

---
+++ src/components/Films/Films.tsx
@@ -0,0 +1,9 @@
+// import {FilmData} from "../../assets/store/storeFilm";
+// import FilmCard from "../FIlmCard/FilmCard";
+//
+// function Films(){
+//     return(
+//
+//     )
+// }
+// export default Films
--- src/pages/Films_page/Films_page.tsx
+++ src/pages/Films_page/Films_page.tsx
@@ -1,6 +1,6 @@
 import  './Films_page.scss'
 import FilmCard from "../../components/FIlmCard/FilmCard";
-import React, {FC, useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {IFilmItem} from "../../assets/types/types";
 import {FilmData} from "../../assets/store/storeFilm";
 import Search from "../../components/filters/Search/Search";
@@ -8,10 +8,10 @@ import SortSelect from "../../components/filters/SortSelect/SortSelect";
 import GenreSelect from "../../components/filters/GenreSelect/GenreSelect";
 import AgeRageSelect from "../../components/filters/AgeRate/AgeRate";
 import ResetBtn from "../../components/filters/ResetBtn/ResetBtn";
-
 import MyModal from "../../components/MyModal/MyModal";
 
-const Films_page: FC = () => {
+
+export default function Films_page() {
     const [FilmList, setFilmList] = useState<IFilmItem[]>([]);
     const [search, setSearch] = useState<string>('');
     const [ageRate, setAgeRate] = useState<string>('');
@@ -19,15 +19,30 @@ const Films_page: FC = () => {
     const [rate, setRate] = useState<string>('');
     const [modal, setModal] = useState<boolean>(false)
 
-    const [year, setYear] = useState<number[]>([2000,2022]);
 
-
+    const marks = [
+        {
+            value: 2000,
+            label: '2000',
+        },
+        {
+            value: 2007,
+            label: '2007',
+        },
+        {
+            value: 2016,
+            label: '2010',
+        },
+        {
+            value: 2022,
+            label: '2022',
+        },
+    ];
 
     useEffect(() => {
         const result = FilmData.filter((item) => {
             return (
                 item.title.toLowerCase().includes(search.toLowerCase().trim()) &&
-                (item.year >= year[0] && item.year <= year[1]) &&
                 item.age.includes(ageRate) &&
                 item.genre.includes(genre)
             );
@@ -40,14 +55,13 @@ const Films_page: FC = () => {
         }
 
         setFilmList(result);
-    }, [search, ageRate, genre, rate, year]);
+    }, [search, ageRate, genre, rate]);
 
     const ResetFilter = () => {
         setSearch('');
         setAgeRate('');
         setGenre('');
         setRate('');
-        setYear([2000,2022])
     };
 
 
@@ -56,38 +70,16 @@ const Films_page: FC = () => {
     const [age, setAge] = useState('')
 
 
-
-    const addNewFilm = () => {
-        event?.preventDefault()
-        const newFilm: IFilmItem = {
-            id: Date.now(),
-            title,
-            description,
-            genre: [],
-            rate: 10,
-            age,
-            year: 20022,
-            img: 'https://media.tenor.com/9ckqI2zyVTkAAAAM/trollge-troll.gif',
-            linkVideo: '',
-        }
-        setFilmList([...FilmData, newFilm])
-        setModal(false)
-        alert('Фильм успешно добавлен!')
-    }
-
-
-// @ts-ignore:
     return (
             <main className="main">
                 <aside className={'filter__bar'}>
                     <p className="filter__text">Фильтр</p>
                     <Search search={search} setSearch={setSearch} />
-
+                    {/*<YearSelect />*/}
                     <SortSelect rate={rate} setRate={setRate} />
                     <GenreSelect genre={genre} setGenre={setGenre}/>
                     <AgeRageSelect ageRate={ageRate} setAgeRate={setAgeRate} />
                     <ResetBtn reset={ResetFilter} />
-                    <button className={'addBtn'} onClick={()=> setModal(true) }>Добавить фильм</button>
                     <MyModal visible={modal} setVisible={setModal}>
                         <form className={'form'}>
                             <p className={'form__title'}>Добавить фильм</p>
@@ -111,7 +103,6 @@ const Films_page: FC = () => {
                                    onChange={event => setAge(event.target.value)}
                             />
 
-                            <button className={'form__btn'} onClick={addNewFilm}>Добавить</button>
                         </form>
                     </MyModal>
                 </aside>
@@ -128,4 +119,3 @@ const Films_page: FC = () => {
             </main>
     )
 }
-export default Films_page
